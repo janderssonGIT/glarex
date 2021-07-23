7853d193b7c3bc5560a2fc367b292edad29fc9d7
Jim Andersson <jim.andersson@knowit.se>
2021-07-23 17:57:54 +0200

    Cleaned up code

--- light_detection_stream.py
+++ light_detection_stream.py
@@ -1,9 +1,14 @@
 # USAGE
-# in (cv) python light_detection_stream.py
+# in .py file folder: /Users/opencv/light_detection_stream
+# - workon cv
+# - deactivate
+# - (cv) python light_detection_stream.py
 
-# Issues:
+# Issue list:
 # TODO If no light source, keep running feed! Fix this loop
 # TODO Make resolution always native to camera
+# TODO Add all operation steps - DONE
+# TODO Add commandline arguments - DONE
 
 # Imports
 from imutils.video import VideoStream
@@ -15,81 +20,166 @@ import argparse
 import imutils
 import time
 import cv2
+import sys, os
 
-MIN_INTENSITY = 205
+MIN_INTENSITY = 225
 MAX_INTENSITY = 255
 
+debugMode = None
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--debug", required=True,
+	help="Activate debugmode or not with true or false")
+args = vars(ap.parse_args())
+
 # Begin video stream, allow the camera sensor to warm up
-# Start the FPS counter
-print("[INFO] starting video stream...")
+# Start FPS counter
+print("[INFO] Begin video stream...")
+print("[INFO] Press 'q' to exit")
 vs = VideoStream(src=0).start()
 time.sleep(2.0)
 fps = FPS().start()
 	
-def interpolateDown(inputframe):
-	return cv2.resize(inputframe, (120, 70), interpolation=cv2.INTER_AREA)
+# "Pixelate" the frame to 128x72
+def interpolate(inputFrame):
+	return cv2.resize(inputFrame, (128, 72), interpolation=cv2.INTER_AREA)
 
-def interpolateUp(inputFrame):
-	return cv2.resize(pixellatedframe, (1280, 720), interpolation=cv2.INTER_NEAREST) 
+# Scale up pixelated frame
+def interpolateSizeUp(inputFrame):
+	return cv2.resize(inputFrame, (1280, 720), interpolation=cv2.INTER_NEAREST)
 
-def thresholdmask(inputframe):
-	thresh = cv2.threshold(inputframe, MIN_INTENSITY, MAX_INTENSITY, cv2.THRESH_BINARY)[1] 				# Threshold the image to reveal lighter regions
-	thresh = cv2.erode(thresh, None, iterations=0) 	#2																						# Perform a series of erosions and dilations to remove any small blobs of noise from the thresholded image
-	thresh = cv2.dilate(thresh, None, iterations=0)	#4
- 
-	cv2.imshow("Mask", thresh)																																		# Show video output of masking (DEBUG PURPOSE)
+# Read arg to global var
+def readDebugArg():
+		global debugMode
+		if args["debug"] == "true":
+			debugMode = True
+		else:
+			debugMode = False
+        
+# Outputs coordinates in debugmode
+def coordinateOutput(i, *c):
+	print("x, y coords for blob #", i)
+	print(*c, sep = ", ") 
+	print("\n")
 
-	labels = measure.label(thresh, background=0) 																									# Perform a connected component analysis on the thresholded image, then initialize a mask to store only the "large" components
-	mask = np.zeros(thresh.shape, dtype="uint8")
+def thresholdmask(inputFrame):
+	threshold = cv2.threshold(inputFrame, MIN_INTENSITY, MAX_INTENSITY, cv2.THRESH_BINARY)[1] 				# Threshold frame to reveal lighter regions
+	threshold = cv2.erode(threshold, None, iterations=0) 	#2																					# Perform erosions and dilations in order to remove any smaller blobs of noise from threshold image
+	threshold = cv2.dilate(threshold, None, iterations=0)	#4
+	
+	if debugMode == True:
+		cv2.imshow("Mask", threshold)																																			# Show video output of masking (DEBUG PURPOSE)
 
-	for label in np.unique(labels): 																															# Loop over the unique components
-		if label == 0: 																																							# If this is the background label, ignore it
+	labels = measure.label(threshold, background=0) 																									# Perform a connected component analysis on the thresholded image, then initialize a mask to store only the "large" components
+	mask = np.zeros(threshold.shape, dtype="uint8")
+
+	for (i, label) in enumerate(np.unique(labels)):
+		# Simple limit to 5 contours
+		if i > 6:
 			continue
-		labelMask = np.zeros(thresh.shape, dtype="uint8") 																					# Otherwise, construct the label mask and count the number of pixels 
+
+		# If this is the background label, ignore it
+		if label == 0:																																								
+			continue
+
+		# Otherwise, construct the label mask and count the number of pixels 
+		labelMask = np.zeros(threshold.shape, dtype="uint8") 																					
 		labelMask[labels == label] = 255
 		numPixels = cv2.countNonZero(labelMask)
 
-		if numPixels > 3 and numPixels < 10000: 																											# If the number of pixels in the component is sufficiently large, then add it to our mask of "large blobs"
+		# If the number of pixels in the component is sufficiently large, then add it to our mask of "large blobs"
+		if numPixels > 1 and numPixels < 2000: 																										
 			mask = cv2.add(mask, labelMask)
 
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)							# Find the contours in the mask, then sort them from left to right
+	# Find the contours in the mask, then sort them from left to right
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)						
 	cnts = imutils.grab_contours(cnts)
 	cnts = contours.sort_contours(cnts)[0]
- 
+  
 	return cnts
 
-# Loop over the frames from the video stream
-# MAIN LOOP
+def wait_key(): # This code could be used when debugMode is FALSE
+	# ''' Wait for a key press on the console and return it. '''
+		result = None
+		if os.name == 'nt':
+			import msvcrt
+			result = msvcrt.getch()
+		else:
+			import termios
+			fd = sys.stdin.fileno()
+
+			oldterm = termios.tcgetattr(fd)
+			newattr = termios.tcgetattr(fd)
+			newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+			termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+			try:
+					result = sys.stdin.read(1)
+			except IOError:
+					pass
+			finally:
+					termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+
+		return result
+
+def OfillMaskAddText(cnts, inputFrame):
+	for (i, c) in enumerate(cnts):
+																					
+		cv2.fillPoly(inputFrame, pts = [c], color=(0,128,0))
+
+		if debugMode == True:
+			coordinateOutput(i, *c)
+
+	inputFrame = interpolateSizeUp(inputFrame)
+	
+	for (i, c) in enumerate(cnts):
+		(x, y, w, h) = cv2.boundingRect(c)																						
+		cv2.putText(inputFrame, "#{}".format(i + 1), ((x*10), (y*10) - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 255, 255), 1)
+
+	return inputFrame
+
+def fillMaskAddText(cnts, inputFrame):
+	for (i, c) in enumerate(cnts):
+																					
+		cv2.fillPoly(inputFrame, pts = [c], color=(0,128,0))
+
+		if debugMode == True:
+			coordinateOutput(i, *c)
+
+	inputFrame = interpolateSizeUp(inputFrame)
+
+	(x, y, w, h) = cv2.boundingRect(c)
+	cv2.putText(inputFrame, "#{}".format(i + 1), ((x*10), (y*10) - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 255, 255), 1)
+
+	return inputFrame
+
 while True:
-	frame = vs.read() 																															# Read a frame from videostream
-	height, width = frame.shape[:2] 																								# Camera resolution
+  # Read command line debug arguments
+	readDebugArg()
+  # Read a frame from videostream
+	frame = vs.read()			
+	# Get camera resolution
+	height, width = frame.shape[:2] 																								
+	# Make the frame grayscale
+	grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	# Apply blur to the greyed frame
+	blurredFrame = cv2.GaussianBlur(grayFrame, (41, 41), 0)
+	# Pixelate frame
+	pixellatedFrame = interpolate(blurredFrame)
+ 	# Detect the brightest regions and return the contours of the frame
+	cnts = thresholdmask(pixellatedFrame) 																					
+	# Add text and color to countours (DEBUG FLAG)
+	pixellatedFrame = fillMaskAddText(cnts, pixellatedFrame)
 
-	grayframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 														# Make the frame grayscale
-	blurredframe = cv2.GaussianBlur(grayframe, (41, 41), 0) 												# Apply blur to the greyed frame
-	pixellatedframe = interpolateDown(blurredframe)
-	cnts = thresholdmask(pixellatedframe) 																					# Detect the brightest regions and return the contours of the frame
-
-	#pixellatedframe = cv2.merge([pixellatedframe,pixellatedframe,pixellatedframe])
-	#pixellatedframe = cv2.cvtColor(pixellatedframe,cv2.COLOR_GRAY2RGB)
-	for (i, c) in enumerate(cnts):																									# Now we draw a circle around the bright areas in the contour image
-		cv2.fillPoly(pixellatedframe, pts = [c], color=(0,128,0))
-		#c2 = np.array(c).flatten()
-		print("x, y coords for blob #", i)
-		print(*c, sep = ", ") 
-		print("\n")
-
-		(x, y, w, h) = cv2.boundingRect(c)																						# Draw the bright spot on the image
-		cv2.putText(pixellatedframe, "#{}".format(i + 1), (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.15, (0, 0, 255), 1) #25 is number of detections
-
-	pixellatedframe = interpolateUp(pixellatedframe)
-	cv2.imshow("Output", pixellatedframe)																						# Show the output frame
+	# Show frame (DEBUG FLAG)
+	if debugMode == True:
+		cv2.imshow("Output", pixellatedFrame)
+														
 	key = cv2.waitKey(1) & 0xFF
 
-	# If `q` key is pressed, break loop
-	if key == ord("q"):
+	if key == ord("q"):																						
 		break
 
-	# Update FPS counter
 	fps.update()
 
 # Stop timer, display FPS
